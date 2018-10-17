c52a987187ba2b4a51a2b1fdfc50f384469ab45f
Jan Cássio <hey@jancassio.com>
2018-10-17 16:42:24 -0400

    expose track resolvers to Mutator and Query types

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,10 +1,12 @@
 import album from './album'
 import artist from './artist'
+import track from './track'
 import user from './user'
 
 const Mutation = {
   ...album.mutation,
   ...artist.mutation,
+  ...track.mutation,
   ...user.mutation,
 }
 
